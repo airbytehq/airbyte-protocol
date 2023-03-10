31779e9510c174573bc57a8f522ba3dc28081728
evantahler <evan@airbyte.io>
2023-03-09 16:32:32 -0800

    load VERSION from .env

--- protocol-models/bin/generate-typescript-classes.sh
+++ protocol-models/bin/generate-typescript-classes.sh
@@ -5,9 +5,18 @@ set -e
 git config --global --add safe.directory /airbyte-protocol
 
 ROOT_DIR=${ROOT_DIR:-$(git rev-parse --show-toplevel)}
+cd $ROOT_DIR
+
+source ".env"
+echo "loaded VERSION=$VERSION"
 
 [ -z "$ROOT_DIR" ] && exit 1
 
 cd protocol-models/typescript
+
+# package.json's vesion MUST be in the file as a literal... so lets use jq to replace it.
+jq ".version=\"$VERSION\"" package.json > package.json.tmp
+mv package.json.tmp package.json
+
 npm install
 npm run build
