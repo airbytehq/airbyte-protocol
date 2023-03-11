3c5379ad8b7e7315d69f0853e2fec3ac7a270a79
evantahler <evan@airbyte.io>
2023-03-10 16:51:23 -0800

    fixup dev versions

--- protocol-models/bin/generate-typescript-classes.sh
+++ protocol-models/bin/generate-typescript-classes.sh
@@ -9,12 +9,13 @@ ROOT_DIR=${ROOT_DIR:-$(git rev-parse --show-toplevel)}
 cd $ROOT_DIR
 
 source ".env"
-echo "loaded VERSION=$VERSION"
+PATCHED_VERSION="${VERSION/.dev/"-dev"}" # npm is strict semver - you only get 2 .s
+echo "loaded VERSION=$PATCHED_VERSION"
 
 cd protocol-models/typescript
 
 npm install -g pnpm
 
-pnpm version $VERSION --allow-same-version
+pnpm version $PATCHED_VERSION --allow-same-version
 pnpm install
 pnpm run build
