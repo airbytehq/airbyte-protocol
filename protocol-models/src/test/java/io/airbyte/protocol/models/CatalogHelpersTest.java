adb61620a232be8a1d1b84357378c68d6b1d472d
Flutra Osmani <flutra@airbyte.io>
2023-12-07 00:37:20 -0800

    remove unused variable

--- protocol-models/src/test/java/io/airbyte/protocol/models/CatalogHelpersTest.java
+++ protocol-models/src/test/java/io/airbyte/protocol/models/CatalogHelpersTest.java
@@ -278,12 +278,6 @@ class CatalogHelpersTest {
 
     final Set<StreamTransform> diff = CatalogHelpers.getCatalogDiff(catalog1, catalog2, configuredAirbyteCatalog);
 
-    final List<StreamTransform> expectedDiff = Stream.of(
-        StreamTransform.createUpdateStreamTransform(new StreamDescriptor().withName(USERS), new UpdateStreamTransform(Set.of(
-            FieldTransform.createRemoveFieldTransform(List.of(DATE), schema1.get(PROPERTIES).get(DATE), false),
-            FieldTransform.createRemoveFieldTransform(List.of("id"), schema1.get(PROPERTIES).get("id"), false)))))
-        .toList();
-
     //configuredCatalog is for a different stream, so no diff should be found
     Assertions.assertThat(diff).hasSize(0);
   }
